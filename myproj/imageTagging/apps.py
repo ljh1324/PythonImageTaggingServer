fdc4676488e59e30fde94e5cf2d63a7816d433dc
Jeonghwan_Lee <leejeonghwan1324@gmail.com>
2018-08-27 09:46:02 +0900

    set enviroment

---
+++ myproj/imageTagging/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImagetaggingConfig(AppConfig):
+    name = 'imageTagging'
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\myproject\창의융합설계프로젝트1\구현\pythonimagetaggingserver\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
